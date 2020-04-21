7fa762cb1f69176188c6687ff0a49eda8526f29a
Jean Jouve <jean.jouve@protonmail.com>
2020-04-21 11:03:58 -0400

    Factorized python script

--- foam_fmm_evaluation.py
+++ foam_fmm_evaluation.py
@@ -5,110 +5,166 @@ import shutil
 import csv
 import itertools
 
-CONFIG_FILE_PATH = pathlib.Path('assets/bubblelattice.txt')
-RESULT_FILE_NAME = 'result.csv'
+class SoapFilmSimulationConfigFile(object):
 
-def getOutputDirectoryTime(output_directory_path):
-    return int(output_directory_path.name[len('output_'):])
+    TRANSLATION = {
+            'subdivisions' : 'mesh-size-n',
+            'size' : 'mesh-size-m'
+            }
 
-def getMostRecentOutputDirectory():
-    most_recent = None
-    for cwd_content in pathlib.Path.cwd().iterdir():
-        if not cwd_content.is_dir():
-            continue
+    @staticmethod
+    def getTranslation(key):
+        return SoapFilmSimulationConfigFile.TRANSLATION.get(
+                key,
+                key)
 
-        if not cwd_content.name.startswith('output_'):
-            continue
+    @staticmethod
+    def getConfigKeyFromVariableName(variable_name):
+        return variable_name.replace('_', '-')
 
-        if (
-                most_recent is None
-                or getOutputDirectoryTime(most_recent)
-                    < getOutputDirectoryTime(cwd_content)):
-            most_recent = cwd_content
+    @staticmethod
+    def getVariableNameFromConfigKey(config_key):
+        return config_key.replace('-', '_')
 
-    return most_recent
+    @classmethod
+    def fromConfigFile(cls, config_file):
+        with open(config_file, newline='') as config_file:
+            config_object = csv.reader(config_file, delimiter=' ')
+            a = dict(config_object)
+            return cls(**a)
 
-def getTimePrefix(method):
-    if method == 'fmmtl':
-        return 'FMMExecution'
-    else:
-        return 'NaiveExecution'
+    def __init__(self, **kwargs):
+        self.config = { self.getConfigKeyFromVariableName(k) : v for k, v in kwargs.items() }
 
-def getMethodSubdirectory(method):
-    if method == 'fmmtl':
-        return 'FMM'
-    else:
-        return 'Naive'
+    def __setattr__(self, name, value):
+        if name == 'config':
+            object.__setattr__(self, name, value)
+        else:
+            self.__setitem__(self.getConfigKeyFromVariableName(name), value)
 
-def getDataPrefixes(method):
-    return [ getTimePrefix(method), 'NumberVertices', 'BoundingBoxVolume', 'NumberFaces' ]
+    def __getattr__(self, name):
+        return self.__getitem__(self.getConfigKeyFromVariableName(name))
 
-argument_parser = argparse.ArgumentParser(
-        description='Run foam experiment of increasing size and record the time took to execute the'
-            'Fast Multipole Method')
-argument_parser.add_argument('-m', '--method', action='append', choices=['fmmtl', 'naive'])
-argument_parser.add_argument('-s', '--save-mesh', action='store_true')
-argument_parser.add_argument('-T', '--save-mesh-period', type=int, default=1)
-argument_parser.add_argument('-o', '--output-directory', default='BubbleLattice')
-argument_parser.add_argument('-c', '--config', default='assets/bubblelattice.txt')
-argument_parser.add_argument('-r', '--resolution', action='append', type=float)
-argument_parser.add_argument('-S', '--size', action='append', type=int)
-argument_parser.add_argument('-t', '--simulation-time', type=float, default=4.0)
-argument_parser.add_argument('--subdivisions', type=int, default=2)
-args = argument_parser.parse_args()
+    def __getitem__(self, key):
+        return self.config[SoapFilmSimulationConfigFile.getTranslation(key)]
 
-if args.method == [] or args.method is None:
-    args.method = ['fmmtl']
+    def __setitem__(self, key, value):
+        self.config[SoapFilmSimulationConfigFile.getTranslation(key)] = value
 
-config = pathlib.Path(args.config).read_text()
-output_directory = pathlib.Path(args.output_directory)
+    def writeToFile(self, filename):
+        with open(filename, mode='w', newline='') as config_file:
+            config_object = csv.writer(config_file, delimiter=' ')
+            for key, value in self.config.items():
+                if isinstance(value, bool):
+                    config_object.writerow((key, str(int(value))))
+                else:
+                    config_object.writerow((key, str(value)))
 
-output_directory.mkdir(parents=True,exist_ok=True)
 
-for size, resolution, method in itertools.product(args.size, args.resolution, args.method):
-    experiment_path = output_directory / f'Size{size}' / getMethodSubdirectory(method)
-    print(f'Starting experiment of size {size}, resolution {resolution} and method {method}.')
-    if experiment_path.exists():
-        print(f'Output directory {experiment_path.as_posix()} was present, deleting.')
-        shutil.rmtree(experiment_path.as_posix())
-    experiment_path.mkdir(parents=True)
 
-    experiment_config_file = experiment_path / 'config.txt'
-    print(f'Saving configuration at {experiment_config_file.as_posix()}')
-    experiment_config_file.write_text(config.format(
-        bubble_lattice_size=size,
-        fmmtl_enable=int(method == 'fmmtl'),
-        output_mesh=int(args.save_mesh),
-        output_mesh_every_n_frames=args.save_mesh_period,
-        output_directory=experiment_path/"output",
-        remeshing_resolution=resolution,
-        simulation_time=args.simulation_time,
-        subdivisions=args.subdivisions))
+class SoapFilmSimulation(object):
 
-    print(f'Starting the simulation')
-    completed_process = subprocess.run(
-            ['./SoapFilm3D', experiment_config_file.as_posix(), 'headless'],
-            capture_output=True,
-            text=True)
+    config_file_name = 'config.txt'
+
+    def __init__(self, output_directory, delete_existing=False, config=None):
+        if config is None:
+            config = SoapFilmSimulationConfigFile()
+        self.config = config
+
+        self.output_directory = pathlib.Path(output_directory)
+        if self.output_directory.exists():
+            if delete_existing:
+                shutil.rmtree(self.output_directory.as_posix())
+            else:
+                raise ValueError(f'The output directory {self.output_directory.as_posix()} exists')
+        self.output_directory.mkdir(parents=True)
+
+        self.config.output_dir = output_directory / 'output'
+        self.writeConfig()
+
+    @property
+    def config_file(self):
+        return self.output_directory / SoapFilmSimulation.config_file_name
+
+    def writeConfig(self):
+        self.config.writeToFile(self.config_file.as_posix())
+
+    def run(self):
+        completed_process = subprocess.run(
+                ['./SoapFilm3D', self.config_file.as_posix(), 'headless'],
+                capture_output=True,
+                text=True)
+
+        if completed_process.returncode != 0:
+            print(completed_process.stderr)
+            raise RuntimeError('An error as occured during the simulation')
+
+        return completed_process.stdout
+
+class SoapFilmSimulationResult(object):
+
+    def __init__(self, output_directory, stdout=None):
+        self.output_directory = pathlib.Path(output_directory)
+        if not self.output_directory.exists():
+            raise ValueError(
+                    f'Output directory f{self.output_directory.as_posix()} does not exists')
         
-    if completed_process.returncode != 0:
-        print('An error as occured during the simulation, skiping')
-        (experiment_path / 'error').touch()
-        continue
+        config_file = self.output_directory / SoapFilmSimulation.config_file_name
+        if not config_file.exists:
+            raise ValueError(f'Config file f{config_file.as_posix()} does not exists')
+        self.config = SoapFilmSimulationConfigFile.fromConfigFile(config_file.as_posix())
 
-    print('Writing result file')
-    data_prefixes = getDataPrefixes(method)
-    data = { data_prefix : [] for data_prefix in data_prefixes }
-    for line in completed_process.stdout.split('\n'):
-        line_components = line.split()
-        if len(line_components) != 2:
-            continue
-        prefix = line_components[0]
-        if prefix in data_prefixes:
-            data[prefix].append(float(line_components[-1]))
+        if stdout is None:
+            stdout = (self.output_directory / 'stdout').read_text()
+        self.stdout = stdout
 
-    with open((experiment_path / 'result.csv').as_posix(), mode='w') as result_file:
-        result_object = csv.writer(result_file, delimiter=' ')
-        result_object.writerow(getDataPrefixes(method))
-        result_object.writerows(zip(*[ data[prefix] for prefix in data_prefixes]))
+if  __name__ == '__main__':
+
+
+    argument_parser = argparse.ArgumentParser(
+            description='Run foam experiment of increasing size and record the time took to execute'
+                'the Fast Multipole Method')
+    argument_parser.add_argument(
+            '-m', '--method', action='append', choices=['fmmtl', 'naive'], default=[])
+    argument_parser.add_argument('-s', '--save-mesh', action='store_true')
+    argument_parser.add_argument('-T', '--save-mesh-period', type=int, default=1)
+    argument_parser.add_argument('-o', '--output-directory', default='BubbleLattice')
+    argument_parser.add_argument('-c', '--config')
+    argument_parser.add_argument('-r', '--resolution', action='append', type=float, default=[])
+    argument_parser.add_argument('-S', '--size', action='append', type=int, default=[])
+    argument_parser.add_argument('-t', '--simulation-time', type=float, default=4.0)
+    argument_parser.add_argument('--scene', default='bubblelattice')
+    argument_parser.add_argument('--subdivisions', type=int, default=2)
+    args = argument_parser.parse_args()
+
+    if args.method == []:
+        args.method = ['fmmtl']
+
+    if args.config is not None:
+        config = SoapFilmSimulationConfigFile.fromConfigFile(args.config)
+    else:
+        config = SoapFilmSimulationConfigFile()
+
+    config.scene = args.scene
+    config.mesh_size_n = args.subdivisions
+    config.output_mesh = args.save_mesh
+    config.output_mesh_every_n_frames=args.save_mesh_period
+    config.simulation_time = args.simulation_time
+
+    for size, resolution, method in itertools.product(args.size, args.resolution, args.method):
+        print(f'Starting experiment of size {size}, resolution {resolution} and method {method}.')
+        experiment_path = (
+                output_directory
+                / f'Resolution{resolution}'
+                / f'Size{size}'
+                / method.capitalize()
+                )
+
+        config.mesh_size_m = size
+        config.remeshing_resolution = resolution
+        config.fmmtl = (method == 'fmmtl')
+
+        simulation = SoapFilmSimulation(experiment_path, delete_existing=True, config=config)
+        stdout = simulation.run()
+        (experiment_path / 'stdout').write_text(stdout)
 
